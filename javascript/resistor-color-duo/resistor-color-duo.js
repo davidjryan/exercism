0d8dc395a3533757634c79bf427f16e8d65addc3
David Ryan <djrdev@icloud.com>
2020-03-25 17:41:22 -0600

    fix: used two maps instead of string conversion

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -5,20 +5,32 @@
 
 export const decodedValue = colors => {
 
-  const colorString = colorMap[colors[0]] + colorMap[colors[1]];
+  const colorString = colorMapTens[colors[0]] + colorMapOnes[colors[1]];
   
-  return parseInt(colorString);
+  return colorString;
 };
 
-const colorMap = {
-    'black': '0',
-    'brown': '1',
-    'red': '2',
-    'orange': '3',
-    'yellow': '4',
-    'green': '5',
-    'blue': '6',
-    'violet': '7',
-    'grey': '8',
-    'white': '9',
+const colorMapTens = {
+    'black': 0,
+    'brown': 10,
+    'red': 20,
+    'orange': 30,
+    'yellow': 40,
+    'green': 50,
+    'blue': 60,
+    'violet': 70,
+    'grey': 80,
+    'white': 90,
+};
+const colorMapOnes = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9,
 };
