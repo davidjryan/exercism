143cbd47ae9e05e5bd944321f0fced9989994422
RB-232 <djrdev@icloud.com>
2020-06-26 15:16:40 -0600

    Done: space-age

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+export const age = (planet, seconds) => {
+  const earthYearInSeconds = 31557600;
+  const age = +((seconds / earthYearInSeconds) / planetEarthYears[planet]).toFixed(2);
+  return age;
 };
+
+const planetEarthYears= {
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'earth': 1.0,
+  'mars': 1.8808158,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus': 84.016846,
+  'neptune': 164.79132
+}
