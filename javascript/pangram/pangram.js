6e75af281c16c1f553b4a58b5823a3c6c0743b0e
RB-232 <djrdev@icloud.com>
2020-07-16 16:16:22 -0600

    passed

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,6 +3,4 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const isPangram = string => /(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)./i.test(string);
