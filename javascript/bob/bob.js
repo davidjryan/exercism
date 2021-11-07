d34e0f398ccc00d7fcc8376ac14f5d8147bb3f1c
RB-232 <djrdev@icloud.com>
2021-11-07 11:56:08 -0700

    got stuck on blank detection

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -2,7 +2,45 @@
 // This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const responces = {
+  question: "Sure.",
+  yell: "Whoa, chill out!",
+  yellQuestion: "Calm down, I know what I'm doing!",
+  blank: "Fine. Be that way!",
+};
+
+function isUpperCase(str) {
+  return str === str.toUpperCase();
+}
+
+function lowerAndUpperUnequal(str) {
+  return str.toLowerCase() != str.toUpperCase();
+}
+
+function isQuestion(str) {
+  return str.slice(-1) === "?";
+}
+
+function isBlank(str) {
+  return str.trim().length === 0;
+}
 
 export const hey = (message) => {
-  throw new Error('Remove this statement and implement this function');
+  if (isBlank(message)) {
+    return responces["blank"];
+  }
+
+  if (isQuestion(message) && isUpperCase(message)) {
+    return responces["yellQuestion"];
+  }
+
+  if (isUpperCase(message) && lowerAndUpperUnequal(message)) {
+    return responces["yell"];
+  }
+
+  if (isQuestion(message)) {
+    return responces["question"];
+  }
+
+  return "Whatever.";
 };
